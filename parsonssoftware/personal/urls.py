72723899ee7ae0fe69780243b32d91d33980301e
Parsons-David <david.parsons@rutgers.edu>
2017-05-22 17:01:37 -0400

    Ported David-Parsons.github.io, except for blog and data sections.

--- parsonssoftware/personal/urls.py
+++ parsonssoftware/personal/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about.html$', views.about, name='about'),
+    url(r'^projects.html$', views.projects, name='projects'),
     url(r'^contact/$', views.contact, name='contact'),
 ]
--- parsonssoftware/personal/views.py
+++ parsonssoftware/personal/views.py
@@ -4,7 +4,13 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'personal/home.html')
+    return render(request, 'personal/index.html')
+
+def about(request):
+    return render(request, 'personal/about.html')
+
+def projects(request):
+    return render(request, 'personal/projects.html')
 
 def contact(request):
     return render(request, 'personal/basic.html', {'content' : ['If you would like to contact me, please email me.', 'david.parsons@rutgers.edu']})
